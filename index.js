0843714a391725c7dabcf22103f8e09f0be7fe46
faith <faithmueni6@gmail.com>
2023-03-23 10:33:10 +0300

    added json server

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+fetch("http://localhost:3000/items")
+  .then(function (response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(data => {
+    
+
+    for(let i=0; i<data.length; i++){
+        console.log(data[i]);
+    const title = document.querySelector('#taskItems h4');
+    const summary = document.querySelector('#taskItems p');
+
+    title.innerText = data[i].text;
+    summary.innerText = data[i].tags;
+    }
+    
+  });
+
+
+
 document.querySelector('#push').onclick = function(){
     if(document.querySelector('#newtask input').value.length == 0){
         alert("Please Enter a Task")
